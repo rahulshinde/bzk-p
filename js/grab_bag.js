70d5d744ff4b79adbd296393faedb5d45d7a6609
Rahul Shinde <rshinde@mica.edu>
2017-02-02 14:03:09 -0500

    editing grab bag

--- js/grab_bag.js
+++ js/grab_bag.js
@@ -1,8 +1,9 @@
 $(document).ready( function(){ 
   $('.image').each(function(){
     var width = Math.floor((Math.random()) * (50 - 20)) + 20;
+    var padding = Math.floor((Math.random()) * (4 - 1)) + 1;
     console.log(width);
-    $(this).css({'width': width + '%'});
+    $(this).css({'width': width + '%', 'padding': padding + "rem"});
   });
       
 })
